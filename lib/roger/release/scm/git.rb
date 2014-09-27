605161b078efe1bb3cdd9e2c7cbbe3c337cfd181
Flurin Egger <flurin@digitpaint.nl>
2014-09-27 13:23:52 +0200

    Properly escape git commands

--- lib/roger/release/scm/git.rb
+++ lib/roger/release/scm/git.rb
@@ -34,11 +34,11 @@ module Roger::Release::Scm
     def get_previous_tag_name
       # Get list of SHA1 that have a ref
       begin
-        sha1s = `git --git-dir=#{git_dir} log --pretty='%H' --simplify-by-decoration`.split("\n")
+        sha1s = `git --git-dir=#{safe_git_dir} log --pretty='%H' --simplify-by-decoration`.split("\n")
         tags = []
         while tags.size < 2 && sha1s.any?
           sha1 = sha1s.shift
-          tag = `git --git-dir=#{git_dir} describe --tags --exact-match #{sha1} 2>/dev/null`.strip
+          tag = `git --git-dir=#{safe_git_dir} describe --tags --exact-match #{sha1} 2>/dev/null`.strip
           tags << tag if !tag.empty?
         end
         tags.last
@@ -51,17 +51,22 @@ module Roger::Release::Scm
       @git_dir ||= find_git_dir(@config[:path])
     end
 
+    # Safely escaped git dir
+    def safe_git_dir
+      Shellwords.escape(self.git_dir.to_s)
+    end
+
     # Some hackery to determine if we're on a tagged version or not
     def get_scm_data(ref = @config[:ref])
       @_version = ""
       @_date = Time.now
       begin
         if File.exist?(git_dir)
-          @_version = `git --git-dir=#{git_dir} describe --tags #{ref} 2>&1`
+          @_version = `git --git-dir=#{safe_git_dir} describe --tags #{ref} 2>&1`
           
           if $?.to_i > 0
             # HEAD is not a tagged verison, get the short SHA1 instead            
-            @_version = `git --git-dir=#{git_dir} show #{ref} --format=format:"%h" -s 2>&1` 
+            @_version = `git --git-dir=#{safe_git_dir} show #{ref} --format=format:"%h" -s 2>&1` 
           else
             # HEAD is a tagged version, if version is prefixed with "v" it will be stripped off
             @_version.gsub!(/^v/,"")
@@ -69,7 +74,7 @@ module Roger::Release::Scm
           @_version.strip!
     
           # Get the date in epoch time
-          date = `git --git-dir=#{git_dir} show #{ref} --format=format:"%ct" -s 2>&1`
+          date = `git --git-dir=#{safe_git_dir} show #{ref} --format=format:"%ct" -s 2>&1`
           if date =~ /\d+/
             @_date = Time.at(date.to_i)
           else
