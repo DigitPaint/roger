2598b45f930a1e3d6cc54f9601f1dbe32ab161bf
Flurin Egger <flurin@digitpaint.nl>
2014-12-22 14:05:21 +0100

    Extract logging into logger interface

--- lib/roger/release.rb
+++ lib/roger/release.rb
@@ -1,8 +1,10 @@
 require File.dirname(__FILE__) + "/cli"
+require File.dirname(__FILE__) + "/logger"
 
 module Roger
   class Release
-    
+    include Roger::Logger
+
     attr_reader :config, :project
     
     attr_reader :finalizers, :injections, :stack, :cleanups
@@ -206,30 +208,6 @@ module Roger
       cleanup! if self.config[:cleanup_build]
       
     end    
-    
-    # Write out a log message
-    def log(part, msg, verbose = false, &block)
-      if !verbose || verbose && self.project.options[:verbose]
-        self.project.shell.say "\033[37m#{part.class.to_s}\033[0m" + " : " + msg.to_s, nil, true
-      end
-      if block_given?
-        begin
-          self.project.shell.padding = self.project.shell.padding + 1
-          yield
-        ensure
-          self.project.shell.padding = self.project.shell.padding - 1
-        end
-      end
-    end
-    
-    def debug(part, msg, &block)
-      self.log(part, msg, true, &block)
-    end
-    
-    # Write out a warning message
-    def warn(part, msg)
-      self.project.shell.say "\033[37m#{part.class.to_s}\033[0m" + " : " + "\033[31m#{msg.to_s}\033[0m", nil, true
-    end
     
     
     # @param [Array] globs an array of file path globs that will be globbed against the build_path
