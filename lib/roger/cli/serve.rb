0169f2a69af51e3febc99663836982addce82467
Flurin Egger <flurin@digitpaint.nl>
2015-01-08 16:25:13 +0100

    Remove :validate option from server

--- lib/roger/cli/serve.rb
+++ lib/roger/cli/serve.rb
@@ -6,23 +6,22 @@ module Roger
 
     class_options :port => :string, # Defaults to 9000
                    :handler => :string, # The handler to use (defaults to mongrel)
-                   :validate => :boolean # Run validation?
 
     def serve
-      
-      server_options = {} 
+
+      server_options = {}
       options.each{|k,v| server_options[k.to_sym] = v }
       server_options[:server] = {}
-      [:port, :handler, :validate].each do |k|
+      [:port, :handler].each do |k|
         server_options[:server][k] = server_options.delete(k) if server_options.has_key?(k)
       end
-      
+
       server = @project.server
       server.set_options(server_options[:server])
-      
+
       puts "Running Roger with #{server.handler.inspect} on port #{server.port}"
-      puts project_banner(@project) 
-      
+      puts project_banner(@project)
+
       server.run!
     end
   end
