51ef0b732bc66c6b923aae553b7dced51e987f48
Flurin Egger <flurin@digitpaint.nl>
2015-06-11 14:44:47 +0200

    Add tests for Processors::Mockup#target_path

---
+++ test/unit/release/mockup_test.rb
@@ -0,0 +1,48 @@
+require "./lib/roger/release.rb"
+require "test/unit"
+
+# Test Roger Mockup
+class MockupTest < ::Test::Unit::TestCase
+  def setup
+    @base = File.dirname(__FILE__) + "/../../project"
+  end
+end
+
+# Test the target_path function of Mockup
+class MockupTargetPathTest < ::Test::Unit::TestCase
+  def setup
+    @base = File.dirname(__FILE__) + "/../../project"
+    @template = Roger::Template.open(@base + "/html/formats/erb.html.erb")
+    @processor = Roger::Release::Processors::Mockup.new
+  end
+
+  def test_with_html_extension
+    assert_equal "bla/test.html", @processor.target_path("bla/test.html", @template).to_s
+    assert_equal "test.html", @processor.target_path("test.html", @template).to_s
+  end
+
+  def test_with_double_extension
+    assert_equal "bla/test.html", @processor.target_path("bla/test.html.erb", @template).to_s
+    assert_equal "test.html", @processor.target_path("test.html.erb", @template).to_s
+    assert_equal "test.html", @processor.target_path("test.html.test", @template).to_s
+  end
+
+  def test_with_unknown_template_mime
+    assert_equal "bla/test.rhtml", @processor.target_path("bla/test.rhtml", @template).to_s
+    assert_equal "test.rhtml", @processor.target_path("test.rhtml", @template).to_s
+  end
+
+  def test_with_template_mime_with_existing_extension
+    template = Roger::Template.open(@base + "/html/formats/csv.rcsv")
+
+    assert_equal "bla/test.csv", @processor.target_path("bla/test.rcsv", template).to_s
+    assert_equal "test.csv", @processor.target_path("test.erb", template).to_s
+  end
+
+  def test_with_template_mime_without_existing_extension
+    template = Roger::Template.open(@base + "/html/formats/csv.rcsv")
+
+    assert_equal "bla/test.csv", @processor.target_path("bla/test", template).to_s
+    assert_equal "test.csv", @processor.target_path("test", template).to_s
+  end
+end
