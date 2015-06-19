7a8fc1d51922c7ba31e668ab40348af8671b5bea
Flurin Egger <flurin@digitpaint.nl>
2015-06-19 12:31:37 +0200

    Make GitBranchFinalizer use the new ReleaseTestCase

--- test/unit/release/finalizers/git_branch_test.rb
+++ test/unit/release/finalizers/git_branch_test.rb
@@ -1,42 +1,33 @@
-require "test_helper"
-require "./lib/roger/release/finalizers/git_branch"
-require "tmpdir"
+require File.dirname(__FILE__) + "/../../../helpers/release_test_case"
+
+require "mocha/test_unit"
 
 module Roger
   # Test for Roger GitBranchFinalizer
-  class GitBranchTest < ::Test::Unit::TestCase
+  class GitBranchTest < ReleaseTestCase
+    self.use_blank_project = true
+
     def setup
-      # Mock git repo
-      @tmp_dir = Pathname.new(Dir.mktmpdir)
-      mock_repo_path = @tmp_dir + "mock_repo"
-      FileUtils.mkdir(mock_repo_path)
-      Dir.chdir(mock_repo_path) do
+      super
+
+      Dir.chdir(project_path.to_s) do
         `git init`
-        `mkdir releases`
-        `touch releases/index.html`
       end
 
-      # Mock release object
-      @release_mock = stub(project: stub(path: mock_repo_path))
+      # Creating a file in build so it will be finalized
+      project_path.file "build/index.html"
 
-      @release_mock.stubs(
-        scm: stub(version: "0.1.999"),
-        log: true,
-        build_path: mock_repo_path.to_s + "/releases"
+      # We're stubbing the scm so we always get the same version.
+      release.stubs(
+        scm: stub(version: "1.0.0")
       )
-    end
-
-    # called after every single test
-    def teardown
-      FileUtils.rm_rf(@tmp_dir)
-      @release_mock = nil
     end
 
     def test_basic_functionality
       git_branch_finalizers = Roger::Release::Finalizers::GitBranch.new
 
       output_dir = git_branch_finalizers.call(
-        @release_mock,
+        release,
         remote: "http://we.aint.go/nna.push.git",
         push: false,
         cleanup: false
@@ -44,7 +35,7 @@ module Roger
 
       Dir.chdir(output_dir + "clone") do
         commit_msg = `git log --pretty=oneline --abbrev-commit`
-        assert_match(/Release 0.1.999/, commit_msg)
+        assert_match(/Release 1.0.0/, commit_msg)
       end
 
       FileUtils.rm_rf(output_dir)
