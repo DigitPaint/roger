840a75549dde380dee589c75f9bd32b9fadd32d3
Edwin van der Graaf <edwin@digitpaint.nl>
2015-08-08 14:43:24 +0200

    Use prompt helpers in rsync to allow --yes flags

--- test/unit/release/finalizers/rsync_test.rb
+++ test/unit/release/finalizers/rsync_test.rb
@@ -48,5 +48,35 @@ module Roger
         finalizer.call(@release)
       end
     end
+
+    def test_rsync_with_question
+      host = "" # Is left empty in test setup
+      question = "Do you wish to upload to #{host}? [y/N]: "
+
+      @release.project.shell.expects(:yes?).with(question).returns(true)
+
+      finalizer = Roger::Release::Finalizers::Rsync.new(
+        remote_path: @target_path.to_s,
+        ask: true
+      )
+
+      finalizer.call(@release)
+
+      assert File.exist?(@target_path + "index.html"), @release.target_path.inspect
+    end
+
+    def test_rsync_with_cli_flag
+      @release.project.options[:yes] = true
+      @release.project.shell.expects(:yes?).never
+
+      finalizer = Roger::Release::Finalizers::Rsync.new(
+        remote_path: @target_path.to_s,
+        ask: true
+      )
+
+      finalizer.call(@release)
+
+      assert File.exist?(@target_path + "index.html"), @release.target_path.inspect
+    end
   end
 end
