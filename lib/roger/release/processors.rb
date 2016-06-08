a2875962134b54c8cb80ed41e5655d591a670146
Flurin Egger <flurin@digitpaint.nl>
2016-06-08 14:32:19 +0200

    Give processors name and own options
    This way processors know about their own identity and options can be
    passed along through the project.options hash. This includes the
    option to disable any processor.

--- lib/roger/release/processors.rb
+++ lib/roger/release/processors.rb
@@ -1,30 +1,70 @@
-module Roger
-  class Release
-    # The Processors namespace
-    module Processors
-      # Abstract Processor class
-      class Base
-        def initialize(options = {})
-          @options = {}
-          @options.update(options) if options
-        end
+# Processors can perform any action on a release
+module Roger::Release::Processors
+  # Abstract Processor class
+  class Base
+    attr_reader :options, :release
 
-        def call(_release, _options = {})
-          fail ArgumentError, "Implement in subclass"
-        end
-      end
+    class << self
+      attr_writer :name
 
-      def self.register(name, processor)
-        fail ArgumentError, "Processor name '#{name.inspect}' already in use" if map.key?(name)
-        fail ArgumentError, "Name must be a symbol" unless name.is_a?(Symbol)
-        map[name] = processor
-      end
-
-      def self.map
-        @_map ||= {}
+      # Name of this processor
+      def name
+        @name || fail(ArgumentError, "Implement in subclass")
       end
     end
+
+    # Default options for this processor
+    def default_options
+      {}
+    end
+
+    # Name of this processor.
+    # - Can be set by setting the :name config in the release block
+    # - Can be overwritten in implementation if needed
+    def name
+      options && options[:name] || self.class.name
+    end
+
+    def call(release, options = {})
+      @release = release
+      @options = {}.update(default_options)
+      @options.update(options) if options
+      @options.update(my_project_options)
+
+      # Stop immideatly if we've been disabled
+      return if @options[:disable]
+
+      perform
+    end
+
+    protected
+
+    # The options passed through the project. This can contain
+    # command line options
+    def my_project_options
+      project_options = release.project.options
+      project_options[:release] && project_options[:release][name] || {}
+    end
+
+    def perform
+      fail ArgumentError, "Implement in subclass"
+    end
+  end
+
+  def self.register(name, processor = nil)
+    if name.is_a?(Class)
+      processor = name
+      name = processor.name
+    end
+    fail ArgumentError, "Processor name '#{name.inspect}' already in use" if map.key?(name)
+    fail ArgumentError, "Name must be a symbol" unless name.is_a?(Symbol)
+    map[name] = processor
+  end
+
+  def self.map
+    @_map ||= {}
   end
 end
+
 require File.dirname(__FILE__) + "/processors/mockup"
 require File.dirname(__FILE__) + "/processors/url_relativizer"
