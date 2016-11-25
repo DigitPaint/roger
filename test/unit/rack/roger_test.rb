a7f6cb999603109751ad525de5a8a75d14becbfb
Flurin Egger <flurin@digitpaint.nl>
2016-11-25 14:56:55 +0100

    Use MockProject for testing Roger Rack middleware

--- test/unit/rack/roger_test.rb
+++ test/unit/rack/roger_test.rb
@@ -1,18 +1,22 @@
 require "test_helper"
-require File.dirname(__FILE__) + "../../../../lib/roger/rack/roger"
 
 module Roger
   module Rack
     # Test Roger Rack
     class ServerTest < ::Test::Unit::TestCase
       def setup
-        @project = Project.new(File.dirname(__FILE__) + "/../../project", rogerfile_path: false)
+        @project = Testing::MockProject.new
         @app = ::Roger::Rack::Roger.new(@project)
       end
 
+      def teardown
+        @project.destroy
+      end
+
       def test_middleware_renders_template
+        @project.construct.file "html/erb.html.erb", "ERB format"
         request = ::Rack::MockRequest.new(@app)
-        response = request.get("/formats/erb")
+        response = request.get("/erb")
 
         assert response.body.include?("ERB format")
       end
