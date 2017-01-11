5869d63cb6d169801350e134b4b783d45d8ac32d
Flurin Egger <flurin@digitpaint.nl>
2017-01-11 17:53:48 +0100

    Don't output anythin in cli noop test

--- test/project/lib/tests/noop/lib/cli.rb
+++ test/project/lib/tests/noop/lib/cli.rb
@@ -1,9 +1,8 @@
 module RogerNoopTest
-  # A CLI command for the Noop test. Doesn't do anything, just output "initialized"
+  # A CLI command for the Noop test. Doesn't do anything
   class Cli < Roger::Test::Cli
     desc "init", "init noop tests"
     def init
-      puts "initialized"
     end
   end
 end
